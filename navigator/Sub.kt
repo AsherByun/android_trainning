c07ca4c95488590a1808f1b510661a3b2900889e
변성재 <47214171+byeunsungjae@users.noreply.github.com>
2020-08-30 20:47:31 +0900

    Add files via upload

---
+++ navigator/Sub.kt
@@ -0,0 +1,12 @@
+package com.example.navigatorkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sub : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
